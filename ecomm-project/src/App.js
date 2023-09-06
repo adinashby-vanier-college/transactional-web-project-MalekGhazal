888a42f11e14f43401cdae83e6e0dca0614b644a
WEI JIN <swissjw@gmail.com>
2023-09-06 16:02:54 -0400

    Add Login page

--- ecomm-project/src/App.js
+++ ecomm-project/src/App.js
@@ -2,12 +2,14 @@ import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import Login from "./pages/Login/Login";
 
 function App() {
   return (
     <div>
-      <Header />
-      <Footer />
+      {/* <Header />
+      <Footer /> */}
+      <Login />
     </div>
   );
 }
