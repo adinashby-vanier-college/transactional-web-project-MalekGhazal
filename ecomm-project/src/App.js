1521a316e38cb3ca9ca226cd512d660ce961894d
WEI JIN <swissjw@gmail.com>
2023-09-06 16:04:52 -0400

    Update Login page

--- ecomm-project/src/App.js
+++ ecomm-project/src/App.js
@@ -2,14 +2,12 @@ import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import Login from "./pages/Login/Login";
 
 function App() {
   return (
     <div>
-      {/* <Header />
-      <Footer /> */}
-      <Login />
+      <Header />
+      <Footer />
     </div>
   );
 }
