de6dfe765ca75bb1e55aa7e5ed4e500addafe030
Malek Kazal <ghazal.malek@gmail.com>
2023-10-01 12:59:40 -0400

    Deploy to Localhost

--- server-side/server.js
+++ server-side/server.js
@@ -1,9 +1,28 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-require("dotenv").config();
+const dotenv = require("dotenv");
+const path = require("path");
+
+dotenv.config();
 
 const app = express();
+
+const _dirman = path.dirname("");
+const buildPath = path.join(_dirman, "../ecomm-project/build");
+
+app.use(express.static(buildPath));
+
+app.get("*", function (req, res) {
+  res.sendFile(
+    path.join(__dirname, "../ecomm-project/build/index.html"),
+    function (err) {
+      if (err) {
+        res.status(500).send(err);
+      }
+    }
+  );
+});
 
 var corsOptions = {
   origin: "http://localhost:3000",
@@ -15,7 +34,7 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 4200;
 
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
   res.json({ message: "Welcome to ECOMM Backend." });
 });
 
