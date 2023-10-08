75bd973af15115dda3e1b0a5e0dc8ba9c29419fd
Malek Kazal <ghazal.malek@gmail.com>
2023-10-08 14:34:55 -0400

    Firestore - cart count

---
+++ ecomm-project/src/components/Cart/CartContext.js
@@ -0,0 +1,36 @@
+import React, { createContext, useContext, useState, useEffect } from "react";
+import { getFirestore, doc, onSnapshot } from "firebase/firestore";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+export const CartContext = createContext();
+
+export const CartProvider = ({ children }) => {
+  const [cart, setCart] = useState([]);
+  const auth = getAuth();
+  const db = getFirestore();
+
+  useEffect(() => {
+    const unsubscribeAuth = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const userDoc = doc(db, "users", user.uid);
+        const unsubscribeFirestore = onSnapshot(
+          userDoc,
+          (doc) => {
+            setCart(doc.data()?.cart || []);
+          },
+          (error) => {
+            console.error("Error fetching cart:", error);
+          }
+        );
+        return () => unsubscribeFirestore();
+      }
+    });
+    return () => unsubscribeAuth();
+  }, [auth, db]);
+
+  return <CartContext.Provider value={cart}>{children}</CartContext.Provider>;
+};
+
+export const useCart = () => {
+  return useContext(CartContext);
+};
